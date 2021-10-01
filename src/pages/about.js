358afe70f566df65efcfad72178ccfcb33fba7e5
Adam Crockett <adam.crockett@incorta.com>
2021-10-01 12:16:04 -0700

    day 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,16 @@
 import * as React from 'react'
+import {Link} from 'gatsby'
+import {Seo} from '../components/seo.js'
 
+ 
 export default function AboutPage() {
     return (
+        <>
+        <Seo title="About This Site" description="More information about this site."/>
         <main>
             <h1>About This Site.</h1>
+            <Link to="/">Back to Home</Link>
         </main>
+        </>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,31 @@
 import * as React from 'react'
+import {Link, useStaticQuery, graphql} from 'gatsby'
+import {Seo} from '../components/seo.js'
 
 export default function IndexPage() {
+    const data = useStaticQuery(graphql`
+    query getSiteTitle {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `)
+
+    const meta = data?.site?.siteMetadata ?? {}
+
+
     return (
+        <>
+        <Seo />
+        <header>
+            <Link to='/'>{meta.title}</Link>
+        </header>
         <main>
             <h1>Hello Frontend Masters!</h1>
+            <Link to="/about">About page</Link>
         </main>
+        </>
     )
 }
