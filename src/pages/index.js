334c9b2b423e7d24e92eabf821373d4f7d0f3fd0
Adam Crockett <adam.crockett@incorta.com>
2021-10-05 17:36:14 -0700

    end review

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,49 @@
 import * as React from 'react'
 import {graphql, Link, useStaticQuery} from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout.js'
+
+import { imageWrapper} from '../styles/index.module.css' 
 
 export default function IndexPage() {
 
     const data = useStaticQuery(graphql`
     query GetBlogPosts {
-  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-    nodes {
-      id
-      slug
-      frontmatter {
-        title
-        date(fromNow: true)
-        description
-      }
-    }
-  }
-}`)
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+            nodes {
+                id
+                slug
+                frontmatter {
+                    title
+                    date(fromNow: true)
+                    description
+                }
+            }
+        }
+        allSanityEpisode(
+            sort: {fields: date, order: DESC}
+            filter: {youtubeID: {ne: ""}}
+            limit: 20
+        ) {
+            nodes {
+                title
+                id
+                guest {
+                    name
+                }
+                gatsbyPath(filePath: "/episode/{sanityEpisode.slug__current}")
+            }
+        }
+    }`)
 
-const posts = data.allMdx.nodes;
-
+    const posts = data.allMdx.nodes;
+    const episodes = data.allSanityEpisode.nodes
 
     return (
         <Layout>
+            <div className={imageWrapper}>
+                <StaticImage src="../images/ivana-la-61jg6zviI7I-unsplash.jpg" alt="a corgi sitting on a bed with red paper hearts all over it. The dog looks unamused." placeholder="dominantColor" width={300} height={300} />
+            </div>
             <h1>Hello Frontend Masters!</h1>
             <Link to="/about">About page</Link>
 
@@ -36,6 +56,16 @@ const posts = data.allMdx.nodes;
                     </li>
                 ))}
             </ul>
+
+            <h2>Latest Episodes of <em>Learn with Jason</em></h2>
+            <ul>
+                {episodes.map((episode) => (
+                    <li key={episode.id}>
+                        <Link to={episode.gatsbyPath}>{episode.title} (with {episode.guest?.[0]?.name})</Link>
+                    </li>
+                ))}
+            </ul>
+            <a href="https://www.learnwithjason.dev"> Watch all episodes of <em>Learn With Jason</em></a>
         </Layout>
     )
 }
